3dc073e611b22b661af3fe32d2654b630595f16f
Alexey Rybakov <alexrybakov@yandex.ru>
2019-10-22 21:56:30 +0300

    enums

---
+++ projects/enums/src/main.rs
@@ -0,0 +1,33 @@
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:#?}", self)
+    }
+}
+
+fn main() {
+    let home = IpAddr::V4(127, 0, 0, 1);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let possible_num: Option<i32> = None;
+
+    println!("{}", possible_num.unwrap_or(1));
+}
